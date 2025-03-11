d166cbe6d820264638a6dfc110015b380b10966f
Vitaly Simonov <guardian909@ya.ru>
2025-03-11 19:42:08 +0300

    Add scope available to products

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,6 @@ class Product < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates :count, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
+
+  scope :available, -> { where("count > ?", 0) }
 end
