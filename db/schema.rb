48d3f099d660d8d6b6a619bb30cbd70327b117c0
Vitaly Simonov <guardian909@ya.ru>
2025-03-11 19:58:43 +0300

    Add migration CreateCarts.

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_11_144230) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_11_165429) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "carts", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_carts_on_user_id"
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name", null: false
