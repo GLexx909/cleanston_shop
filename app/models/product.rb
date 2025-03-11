caeb05748f8ae0847a6f20e8f630987fdcfe2aeb
Vitaly Simonov <guardian909@ya.ru>
2025-03-11 20:56:14 +0300

    Add dependencies between cart_item, product and cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
+  has_many :cart_item, dependent: :destroy
+
   validates :name, presence: true, uniqueness: true
   validates :count, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
